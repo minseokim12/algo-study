2ab674adf687eaa4d79032d5d83c3ee126950e9f
colin kang <colin.kt.kang@gmail.com>
2022-03-03 16:59:16 +0900

    colin 243 python

---
+++ leetcode/234/colinkang.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        words = []
+        current = head
+        while (current is not None):
+            words.append(current.val)
+            current = current.next
+        #return vals == vals[::-1]
+        wordsreverse = words[:]
+        wordsreverse.reverse()
+        return words == wordsreverse
